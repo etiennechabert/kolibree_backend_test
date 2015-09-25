16959c0dd4293aa082515b60e16711bf7c4bdc09
Etienne Chabert <etienne.chabert@gmail.com>
2015-09-25 23:31:26 +0200

    Definition of my models done

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(blank=False)
+    last_name = models.CharField(blank=False)
+    date_of_birth = models.DateField(blank=False)
+    email = models.EmailField(unique=True)
+    password = models.CharField(blank=False)
+
+class UserToken(models.Model):
+    user_id = models.ForeignKey('User')
+    token = models.UUIDField(auto=True)
+    creation_dateTime = models.DateTimeField(auto_now=True)
