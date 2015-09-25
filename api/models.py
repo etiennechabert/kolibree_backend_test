16fca51ebc80aec13a0c02a068247fa4613b3c46
Etienne Chabert <etienne.chabert@gmail.com>
2015-09-26 00:12:12 +0200

    Working migration ... finaly

--- api/models.py
+++ api/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 class User(models.Model):
-    first_name = models.CharField(blank=False)
-    last_name = models.CharField(blank=False)
-    date_of_birth = models.DateField(blank=False)
-    email = models.EmailField(unique=True)
-    password = models.CharField(blank=False)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField
+    email = models.EmailField(unique=True, blank=False)
+    password = models.CharField(max_length=100)
 
 class UserToken(models.Model):
-    user_id = models.ForeignKey('User')
-    token = models.UUIDField(auto=True)
+    user = models.ForeignKey('User')
+    token = models.UUIDField(auto_created=True)
     creation_dateTime = models.DateTimeField(auto_now=True)
