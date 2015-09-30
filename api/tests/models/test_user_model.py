4217c3cf95fe7c66147541f0b1147abc38e5913a
Etienne Chabert <etienne.chabert@gmail.com>
2015-09-30 08:27:03 +0200

    Add a new test for user duplication

--- api/tests/models/test_user_model.py
+++ api/tests/models/test_user_model.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from django.core.exceptions import ValidationError
+from django.db import IntegrityError
 from api.models.user import User
 
 class userTestCase(TestCase):
@@ -21,5 +22,12 @@ class userTestCase(TestCase):
             self.user.clean_fields()
 
     def test_user_model_success(self):
+        User.objects.all().delete();
         user = User.objects.create(first_name='Etienne', last_name='Chabert', email='etienne.chabert@gmail.com', password='pass', date_of_birth='1990-11-18')
         self.assertEqual(user.full_clean(), None)
+
+    def test_user_model_duplicate(self):
+        User.objects.all().delete();
+        with self.assertRaises(IntegrityError):
+            user = User.objects.create(first_name='Etienne', last_name='Chabert', email='etienne.chabert@gmail.com', password='pass', date_of_birth='1990-11-18')
+            user = User.objects.create(first_name='Etienne', last_name='Chabert', email='etienne.chabert@gmail.com', password='pass', date_of_birth='1990-11-18')
